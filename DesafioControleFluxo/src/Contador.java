2a381b39d4673f65e05ba4e56cd4d5783e7a1fd6
Helldraw <116988027+Helldraw@users.noreply.github.com>
2023-09-14 08:45:13 -0300

    Adicionado DesafioControleFluxo
    Projeto do desafio de controle de fluxo criado e adicionado com as classes Contador e ParametrosInvalidosException

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Informe o primeiro número do parametro: ");
+		int parametroUm = entrada.nextInt();
+		System.out.println("Informe o segundo número do parametro");
+		int parametroDois = entrada.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+				
+		}catch (ParametrosInvalidosException e) {
+			System.out.println("O segundo parametro deve ser maior que o primeiro parametro" + e);
+		}
+		entrada.close();
+		
+	}
+	
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+		}
+		int contagem = parametroDois - parametroUm;
+		for(int indice = 0; indice < contagem; indice++) {
+			System.out.println(indice+1);
+	    }
+
+		
+	}
+
+}
